44d39d87091c35b1d133c49a282040952f2d6c28
safikolislam <safikolislam72@gmail.com>
2025-03-05 15:32:25 +0600

    first commit

---
+++ script/theme-btn.js
@@ -0,0 +1,6 @@
+document.getElementById('random-background').addEventListener('click',function(){
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+   document.body.style.background =  "rgb(" + red + ", " + green + ", " + blue + ")" ;
+})
