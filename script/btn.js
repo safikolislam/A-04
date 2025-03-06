2c6855343ced6c59419d920de76fc7b7eb99adca
safikolislam <safikolislam72@gmail.com>
2025-03-06 11:20:57 +0600

    added the historyitems

--- script/btn.js
+++ script/btn.js
@@ -10,18 +10,24 @@ for (let i = 0; i < buttons.length; i++) {
 
     buttons[i].onclick = function () {
         alert("Board updated successfully"); 
+
         buttons[i].disabled = true; 
         buttons[i].style.backgroundColor = "gray"; 
-
+      
         sum -= 1; 
         subtract += 1; 
 
         sumElement.textContent = sum; 
         subtractElement.textContent = subtract;
-       
+        const container = document.getElementById('Activity')
+        const div = document.createElement('div');
+        div.innerText=`completed The task`
+       container.appendChild(div)
+        
         if(sum===0){
             alert('Congrates!!!You have completed all the current task')
         }
+      
     };
 }
 
@@ -29,3 +35,6 @@ for (let i = 0; i < buttons.length; i++) {
 
 
 
+document.getElementById('Clear-History').addEventListener('click',function(){
+    document.getElementById('Activity').innerHTML = '';
+})
